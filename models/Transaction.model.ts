0c2f32f79ee7ceb4c7cef4fa5af19c66785f0668
Kevin Truong <kevintruong6587@gmail.com>
2025-02-14 13:04:07 +0700

    Update timestamp

--- models/Transaction.model.ts
+++ models/Transaction.model.ts
@@ -1,26 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 import { ITransaction } from "$types/transaction.interface";
 
-const transactionSchema = new Schema<ITransaction>({
-  customerId: {
-    type: Schema.Types.ObjectId,
-    ref: "Account",
-    required: true,
+const transactionSchema = new Schema<ITransaction>(
+  {
+    customerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    appointmentId: {
+      type: Schema.Types.ObjectId,
+      ref: "Appointment",
+      required: true,
+    },
+    paymentMethodId: {
+      type: Schema.Types.ObjectId,
+      ref: "PaymentMethod",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
   },
-  appointmentId: {
-    type: Schema.Types.ObjectId,
-    ref: "Appointment",
-    required: true,
-  },
-  paymentMethodId: {
-    type: Schema.Types.ObjectId,
-    ref: "PaymentMethod",
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model<ITransaction>("Transaction", transactionSchema);
--- models/UserQuiz.model.ts
+++ models/UserQuiz.model.ts
@@ -1,38 +1,41 @@
 import mongoose, { Schema } from "mongoose";
 import { IUserQuiz } from "$types/userquiz.interface";
 
-const userQuizSchema = new Schema<IUserQuiz>({
-  accountId: {
-    type: Schema.Types.ObjectId,
-    ref: "Account",
-    required: true,
-  },
-  scoreBandId: {
-    type: Schema.Types.ObjectId,
-    ref: "Scoreband",
-    required: true,
-  },
-  questionResult: {
-    type: [
-      {
-        questionId: {
-          type: Schema.Types.ObjectId,
-          ref: "Question",
-          required: true,
+const userQuizSchema = new Schema<IUserQuiz>(
+  {
+    accountId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    scoreBandId: {
+      type: Schema.Types.ObjectId,
+      ref: "Scoreband",
+      required: true,
+    },
+    questionResult: {
+      type: [
+        {
+          questionId: {
+            type: Schema.Types.ObjectId,
+            ref: "Question",
+            required: true,
+          },
+          answerId: {
+            type: [Schema.Types.ObjectId],
+            ref: "Answer",
+            required: true,
+          },
         },
-        answerId: {
-          type: [Schema.Types.ObjectId],
-          ref: "Answer",
-          required: true,
-        },
-      },
-    ],
-    required: true,
+      ],
+      required: true,
+    },
+    totalPoint: {
+      type: Number,
+      required: true,
+    },
   },
-  totalPoint: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model<IUserQuiz>("UserQuiz", userQuizSchema);
